1c7e796fe46f71d04ecbf6a40d8b7eb8772ac70e
Marlon Elpedes <marlon.elpedes@yahoo.com>
2019-02-28 20:53:03 +0800

    Arcanys Drag and Drop using DropZoneJs

---
+++ DragAndDrapMVC/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DragAndDrapMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult FileUpload(HttpPostedFileBase file)
+        {
+            try
+            {
+                var memStream = new MemoryStream();
+                file.InputStream.CopyTo(memStream);
+
+                byte[] fileData = memStream.ToArray();
+
+                //save file to database using fictitious repository
+                //var repo = new FictitiousRepository();
+                //repo.SaveFile(file.FileName, fileData);
+            }
+            catch (Exception exception)
+            {
+                return Json(new
+                {
+                    success = false,
+                    response = exception.Message
+                });
+            }
+
+            return Json(new
+            {
+                success = true,
+                response = "File uploaded."
+            });
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ DragAndDrapMVC/Models/UploadViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DragAndDrapMVC.Models
+{
+    public class UploadViewModel
+    {
+        public string UserEmail { get; set; }
+        public int FileSizeLimitMB { get; set; }
+        public int MaxFiles { get; set; }
+        public string ContentTypes { get; set; }
+    }
+}
